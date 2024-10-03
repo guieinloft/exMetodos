cd48b8db041e8f7c3b0eca698ab0e1d3ca028d49
Tiago Steffler <tiago.steffler@acad.ufsm.br>
2024-10-03 17:03:00 -0300

    horny

--- M02/horner.c
+++ M02/horner.c
@@ -20,38 +20,39 @@ polynomial create_p(int degree, ...){
 	va_list args;
 	va_start(args, degree);
 	p.coeficients = (double*)malloc(sizeof(double) * (degree + 1));
+	printf("P(x): ");  
 	for(int i = 0; i <= degree; i++){
 		p.coeficients[i] = va_arg(args, double);
-		printf("%lf\t", p.coeficients[i]);
+		printf("%.9E \t ", p.coeficients[i]);
 	}
 	va_end(args);
-	printf("\n");
+	printf("\n\n");
 	return p;
 }
 
-polynomial find_b0(polynomial p, double x0){
+polynomial find_b(polynomial p, double x0, int k){
 	polynomial b;
 	b.degree = p.degree;
 	b.coeficients = (double*)malloc(sizeof(double) * (b.degree + 1));
 	b.coeficients[0] = p.coeficients[0];
-	printf("b: %lf\t", b.coeficients[0]);
+	printf("b%d: %.9E \t", k, b.coeficients[0]);
 	for(int i = 1; i <= b.degree; i++){
 		b.coeficients[i] = p.coeficients[i] + b.coeficients[i-1] * x0;
-		printf("%lf\t", b.coeficients[i]);
+		printf("%.9E \t", b.coeficients[i]);
 	}
 	printf("\n");
 	return b;
 }
 
-polynomial find_c1(polynomial b, double x0){
+polynomial find_c(polynomial b, double x0, int k){
 	polynomial c;
 	c.degree = b.degree - 1;
 	c.coeficients = (double*)malloc(sizeof(double) * (c.degree + 1));
 	c.coeficients[0] = b.coeficients[0];
-	printf("c: %lf\t", c.coeficients[0]);
+	printf("c%d: %.9E \t", k, c.coeficients[0]);
 	for(int i = 1; i <= c.degree; i++){
 		c.coeficients[i] = b.coeficients[i] + c.coeficients[i-1] * x0;
-		printf("%lf\t", c.coeficients[i]);
+		printf("%.9E \t", c.coeficients[i]);
 	}
 	printf("\n");
 	return c;
@@ -61,25 +62,25 @@ polynomial find_c1(polynomial b, double x0){
 
 
 int main(){
-	int i = 0;
+	int i = 1;
 	double x, x_prev, f_x, df_x, er;
 
-	polynomial p = create_p(3, 1.0, -2.0, -4.0, 4.0);
+	polynomial p = create_p(5, 1.0, 0.725616, -18.8815, -0.872976, 53.87, -16.4925);
 	polynomial b;
 	polynomial c;
 
 	x = 0.5;
-	printf("%d: %lf\n\n", i, x);
+	printf("x%d: %.9E \n\n", 0, x);
 	do{
 		x_prev = x;
-		b = find_b0(p, x);
-		c = find_c1(b, x);
+		b = find_b(p, x, i);
+		c = find_c(b, x, i);
 		if (b.coeficients[b.degree] == 0.0) break;
 		x = x_prev - b.coeficients[b.degree]/c.coeficients[c.degree];
 		er = fabs(x - x_prev)/fabs(x);
-		printf("%d: %lf \t %lf\n\n", i, x, er);
+		printf("x%d: %.9E \t%.9E \n\n", i, x, er);
 		i++;
 	}
-	while(er > PREC);
+	while(er > pow(10, -6));
 	return 0;
 }
