8d0dd0d0e8a5471541e4520339a6cf6523097236
visitante <visitante@lenovo-m70q-i5-01.inf.ufsm.br>
2024-10-14 17:27:11 -0300

    egpt

--- M03/egpt.c
+++ M03/egpt.c
@@ -18,13 +18,16 @@ void troca_linha(double a[N][N+1], int l1, int l2){
 	}
 }
 
-void troca_coluna(double a[N][N+1], int l1, int l2){
+void troca_coluna(double a[N][N+1], x *xarray[N], int l1, int l2){
 	double temp;
 	for(int i = 0; i < N; i++){
 		temp = a[i][l1];
 		a[i][l1] = a[i][l2];
 		a[i][l2] = temp;
 	}
+	int itemp = xarray[l1].k;
+	xarray[l1].k = xarray[l2].k;
+	xarray[l2].k = itemp;
 }
 
 void start_xarray(x *xarray[N]){
@@ -32,7 +35,7 @@ void start_xarray(x *xarray[N]){
 }
 
 int main(){
-	int i, j, k, lmax;
+	int i, j, k, lmax, cmax;
 	double mult, max;
 	double a[N][N+1] = {{-17.0, 19.0, -3.0, 8.0, 86.0},
 			{13.0, -10.0, -18.0, -20.0, -297.0},
@@ -48,13 +51,16 @@ int main(){
 		lmax = i;
 		//encontrando valor maximo
 		for(j = i; j < N; j++){
-			//todo
-			if(fabs(a[j][i]) > max){
-				lmax = j;
-				max = fabs(a[j][i]);
+			for(k = i; k < N; k++){
+				if(fabs(a[j][k]) > max){
+					lmax = j;
+					cmax = k;
+					max = fabs(a[j][k]);
+				}
 			}
 		}
 		if(lmax != i) troca_linha(a, i, lmax);
+		if(cmax != i) troca_coluna(a, xarray, i, cmax);
 		for(j = i+1; j < N; j++){
 			mult = a[j][i] / a[i][i];
 			a[j][i] = 0.0;
@@ -71,11 +77,11 @@ int main(){
 
 	//resolvendo os sistemas formados
 	for(i = N-1; i > -1; i--){
-		x[i] = a[i][N];
+		xarray[i].v = a[i][N];
 		for(j = N-1; j > i; j--){
-			x[i] -= a[i][j] * x[j];
+			xarray[i].v -= a[i][j] * xarray[j].v;
 		}
-		x[i] /= a[i][i];	
+		xarray[i].v /= a[i][i];	
 	}
 	for(i = 0; i < N; i++){
 		printf("%+.10E ", x[i]);
