b3041bc89bde982197ceee113ff9298111509b7d
Guilherme Einloft <guieinloft@proton.me>
2024-11-16 19:11:44 -0300

    ordenando o array

--- M04/newton_gr.c
+++ M04/newton_gr.c
@@ -32,68 +32,64 @@ void get_dd(double mat[N][N+1], int col){
 }
 
 void print_mat(double mat[N][N+1]){
-    printf("           x           y");
-    for(int i = 1; i < N-1; i++) printf("         DD%d", i);
-    printf("         DD%d\n", N-1);
+    printf("x,y,");
+    for(int i = 1; i < N-1; i++) printf("DD%d,", i);
+    printf("DD%d\n", N-1);
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N+1; j++){
-            if(j > N-i) printf("         ---");
-            else printf("% 12.6g", mat[i][j]);
+            if(j > N-i) printf("-------");
+            else printf("%+.6g", mat[i][j]);
+            if(j < N) putchar(',');
         }
         putchar('\n');
     }
 }
 
-void get_pz(double pz[N][2], double mat[N][N+1], double z, int i){
-    pz[i][0] = mat[0][i+1];
+void get_pz(double pz[N], double mat[N][N+1], double z, int i){
+    pz[i] = mat[0][i+1];
     for(int j = 0; j < i; j++){
-        pz[i][0] *= (z - mat[j][0]);
+        pz[i] *= (z - mat[j][0]);
     }
-    if(i != 0){
-        pz[i][0] += pz[i-1][0];
-        pz[i][1] = fabs(pz[i][0] - pz[i-1][0])/fabs(pz[i][0]);
+    if(i != 0) pz[i] += pz[i-1];
+}
+
+void print_pz(double pz[1000][N]){
+    for(int i = 0; i < 1000; i++){
+        for(int j = 0; j < N; j++){
+            printf("%+.6g,", i, pz[i][j]);
+        }
+        putchar('\n');
     }
 }
 
-int get_approx(double pz[N][2]) {
-    int k = 2;
-    for (int i = 3; i < N; i++) {
-        if (pz[i][1] < pz[k][1]) k = i;
-        else return k;
-    }
-}
-
-void print_pz(double pz[N][2], int k){
-    int index;
-
-    printf("k        Pk(z)          ER\n");
-    for(int i = 0; i < N; i++){
-        printf("%d % 12.6g", i, pz[i][0]);
-        if(i > 0) printf("% 12.6g\n", pz[i][1]);
-        else printf("         ---\n");
-    }
+void plot_csv(int n, double pz[1000][N]){
+    char fname[32];
+    FILE *file;
+    sprintf(fname, "plot_%02d.dat", n);
+    file = fopen(fname, "w");
+    for(int i = 0; i < 1000; i++) fprintf(file, "%.6g %.6g\n", 2.0*i/1000, pz[i][n]);
+    fclose(file);
 }
 
 int main(){
     double mat[N][N+1];
     double z;
-    double pz[N][2];
-    int k;
+    double pz[1000][N];
     fill_x_y(mat);
     scanf("%lf", &z);
     order_x_y(mat, z);
     for(int i = 2; i < N+1; i++){
         get_dd(mat, i);
     }
-    printf("Tabela de Diferenças Divididas:\n");
     print_mat(mat);
     putchar('\n');
-    for(int i = 0; i < N; i++){
-        get_pz(pz, mat, z, i);
+    for(int i = 0; i < 1000; i++){
+        for(int j = 0; j < N; j++){
+            get_pz(pz[i], mat, 2.0*i/1000, j);
+        }
     }
-    k = get_approx(pz);
-    printf("Aproximações:\n");
-    print_pz(pz, k);
-    printf("Aproximação mais confiável: %d: %.6g\n", k, pz[k][0]);
+    for(int i = 0; i < N; i++){
+        plot_csv(i, pz);
+    }
     return 0;
 }
